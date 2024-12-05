3e5383772ee857e3fb1faa0f6d16e8b7f2f6b0b5
Nick-Major <divanchik26@gmail.com>
2024-12-05 14:43:41 +0300

    Tests fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,19 @@ import tseslint from "typescript-eslint";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  {languageOptions: { globals: {...globals.browser, ...globals.jest, ...globals.node } },
+  ignores: [
+    'node_modules/',
+    '**/node_modules/',
+    '/**/node_modules/*',
+    'out/',
+    'dist/',
+    'build/',
+  ]},
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.config({
+    rules: {
+      "@typescript-eslint/no-require-imports": "off"
+    }
+  }),
 ];
